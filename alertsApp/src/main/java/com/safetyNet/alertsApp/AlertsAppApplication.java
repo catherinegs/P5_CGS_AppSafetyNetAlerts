d6a6901de44329524b6de50f57855016d0efca80
catherinegs <catherine.gourdinsg@gmail.com>
2022-09-13 20:56:33 +0200

    safetyAlertsApp
    new project java spring

---
+++ alertsApp/src/main/java/com/safetyNet/alertsApp/AlertsAppApplication.java
@@ -0,0 +1,13 @@
+package com.safetyNet.alertsApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlertsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlertsAppApplication.class, args);
+	}
+
+}
---
+++ alertsApp/src/test/java/com/safetyNet/alertsApp/AlertsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safetyNet.alertsApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlertsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
